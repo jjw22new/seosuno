cfc30c95799d2f509afb67f856f7f662ffb59b1d
Jung JaeYeol <>
2026-02-10 11:39:08 +0900

    init

---
+++ generate.js
@@ -0,0 +1,31 @@
+const https = require("https");
+const fs = require("fs");
+
+const RSS_URL = "https://blog.rss.naver.com/seosuno.xml";
+
+https.get(RSS_URL, (res) => {
+  let data = "";
+
+  res.on("data", (chunk) => (data += chunk));
+
+  res.on("end", () => {
+    const links = [...data.matchAll(/<link>(.*?)<\/link>/g)]
+      .map((m) => m[1])
+      .filter((url) => url.includes("blog.naver.com"));
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${links
+  .map(
+    (url) => `
+  <url>
+    <loc>${url}</loc>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+    fs.writeFileSync("sitemap.xml", sitemap);
+    console.log("✅ sitemap.xml 생성 완료");
+  });
+});
